6dac81ebbbd9abfee36cbd3bc6349b291c0eb723
forspam <forspam111@mail.ru>
2018-11-25 14:18:44 +0300

    Initial Commit

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by Иван on 25/11/2018.
+//  Copyright © 2018 Иван. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
